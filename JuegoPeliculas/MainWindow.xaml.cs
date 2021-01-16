7e20b05edb348084c036fadb90cd81c3a803d910
Juan Manuel Garcia <draconigro@gmail.com>
2021-01-16 22:57:22 +0100

    Faltan los Binding y casi ya

--- JuegoPeliculas/MainWindow.xaml.cs
+++ JuegoPeliculas/MainWindow.xaml.cs
@@ -15,10 +15,9 @@ namespace JuegoPeliculas
     /// </summary>
     public partial class MainWindow : Window
     {
-        //quitar, solo para hacer pruebas.
-       // Peliculas pelicula = new Peliculas("metropolis", "Mujer bionica blanco y negro", "https://www.experimenta.es/wp-content/uploads/2018/03/metropolis-la-boca-experimenta-02-800x1200.jpg" ,2, "ciencia-ficcion");
-       // List<Peliculas> peliculas=new List<Peliculas>();  //quitar, solo para hacer pruebas.
 
+       // Peliculas pelicula = new Peliculas("metropolis", "Mujer bionica blanco y negro", "https://www.experimenta.es/wp-content/uploads/2018/03/metropolis-la-boca-experimenta-02-800x1200.jpg" ,2, "ciencia-ficcion");
+      
         ObservableCollection<Peliculas> cincoPeliculas=new ObservableCollection<Peliculas>(); //iniciar las peliculas del juego
         ObservableCollection<Peliculas> listaPeliculas=new ObservableCollection<Peliculas>();
         Juego juego=new Juego();
@@ -28,11 +27,11 @@ namespace JuegoPeliculas
             InitializeComponent();
             // peliculas.Add(pelicula); //quitar, solo para hacer pruebas.
             // peliculas = Peliculas.GuardarPeliculas(); //usar cuando este arreglado.
-           // listaPeliculas.Add(pelicula);
-            posterImage.DataContext = listaPeliculas;
+          
+            juegoPeliculasGrid.DataContext = cincoPeliculas;
 
 
-            JuegoDockPanel.DataContext = listaPeliculas;
+           //  JuegoDockPanel.DataContext = listaPeliculas;
            // JuegoStackPanel.DataContext = listaPeliculas;
 
             puntosTotalesTextBox.DataContext = juego;// para los puntos del juego que no va
@@ -44,14 +43,15 @@ namespace JuegoPeliculas
 
         private void NuevaPartidaButton_Click(object sender, RoutedEventArgs e)
         {
-            if (listaPeliculas.Count() > 5)
+
+            if (listaPeliculas.Count() >= 5)
             {
-                textNumeroPaginauno.Text = "1";
-                textNumeroPagina.Text = cincoPeliculas.Count.ToString();//no peta pero no funciona
+             textNumeroPaginauno.Text = "1";
+            cincoPeliculas = juego.InicarJuego(listaPeliculas);
+            //selecionar 5 peliculas aleatorias.(esto ya esta hecho en un principio)
+            textNumeroPagina.Text = cincoPeliculas.Count.ToString();
 
-                cincoPeliculas = juego.InicarJuego(listaPeliculas);
-                //selecionar 5 peliculas aleatorias.(esto ya esta hecho en un principio)
-
+     
             }
             else
                 MessageBox.Show("No tenemos peliculas para jugar", "Juego peliculas", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -63,12 +63,12 @@ namespace JuegoPeliculas
                 openFileDialog.Filter = "JSON file (*.JSON)|*.JSON"; //ya funciona
             if (openFileDialog.ShowDialog() == true)
             {
-                using (StreamReader jsonStream = File.OpenText("personas.json"))
+                using (StreamReader jsonStream = File.OpenText(openFileDialog.FileName))
                 {
                     var json = jsonStream.ReadToEnd();
-                    listaPeliculas = JsonConvert.DeserializeObject<ObservableCollection<Peliculas>>(json);
+                List<Peliculas> nuevaListaPeliculas = JsonConvert.DeserializeObject<List<Peliculas>>(json);
 
-                    foreach (Peliculas p in listaPeliculas)
+                    foreach (Peliculas p in nuevaListaPeliculas)
                     {
                         listaPeliculas.Add(p);
                     }
@@ -91,21 +91,23 @@ namespace JuegoPeliculas
         private void darPistaCheckBox_Checked(object sender, RoutedEventArgs e)
         {
             //se puede hacer con trigger pero como tengo que controlar los puntos veo mejor hacerlo aqui.
-            darPixtaTextBlok.Visibility = Visibility;
+            darPixtaTextBlok.Visibility = Visibility.Visible;
             juego.pistaVista = true; //Peta TODO arreglar
           //   juego.CalcularPuntos(pelicula.nivelDificultad); //mirar que esta mal, TODO
         }//falta algo
 
         private void validadPeliculaButton_Click(object sender, RoutedEventArgs e)
-        { 
+        {
+            int numero = Int32.Parse(textNumeroPaginauno.Text); 
+
             //pasar a minusculas el texto que entra y el titulo o buscar solucion
-           /* if (validarTituloTextBox.Text == cincoPeliculas.Count(validarTituloTextBox.Text))
+           if (validarTituloTextBox.Text == cincoPeliculas[numero].titulo)
             {
                 MessageBox.Show("Pelicula Correcta", "Juego peliculas", MessageBoxButton.OK, MessageBoxImage.Information);
-                juego.CalcularPuntos(pelicula.nivelDificultad);
+              //  juego.CalcularPuntos(pelicula.nivelDificultad);
                 // pasar a siguiente pelicula, TODO
             }
-            else*/
+        //    else*/
                 MessageBox.Show("Pelicula erronea" , "Juego peliculas", MessageBoxButton.OK, MessageBoxImage.Information);
         } //falta algo
 
@@ -113,7 +115,6 @@ namespace JuegoPeliculas
         {
             if (dentro.SelectedItem==null)
             {
-
                 listaPeliculas.Add((Peliculas)this.Resources["nuevaPeli"]);
                 MessageBox.Show("Pelicula añadida", "Juego peliculas", MessageBoxButton.OK, MessageBoxImage.Information);
             }
@@ -124,7 +125,7 @@ namespace JuegoPeliculas
         private void ReiniciarPeliculas()
         {
             this.Resources.Remove("nuevaPeli");
-            this.Resources.Add("nuevaPeli", new Peliculas());
+            this.Resources.Add("nuevaPeli", new Peliculas());//no deja los campos en blanco
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
@@ -132,6 +133,41 @@ namespace JuegoPeliculas
             listaPeliculas.Remove((Peliculas)dentro.SelectedItem);
             ReiniciarPeliculas();
 
+        }
+ 
+        private void ImagenFlechaIzq_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int actual = Int32.Parse(textNumeroPaginauno.Text);
+
+            if (actual > 1)
+            {
+                juegoPeliculasGrid.DataContext = cincoPeliculas[actual - 2];
+                textNumeroPaginauno.Text = (actual - 1).ToString();
+            }
+        }
+
+        private void ImagenFlechaDer_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int actual = Int32.Parse(textNumeroPagina.Text);
+
+            if (actual < cincoPeliculas.Count)
+            {
+                juegoPeliculasGrid.DataContext = cincoPeliculas[actual];
+                textNumeroPagina.Text = (actual + 1).ToString();
+            }
+        }
+
+        private void ExaminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG file (*.png)|*.PNG"; //ya funciona
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (StreamReader imagenStream = File.OpenText(openFileDialog.FileName))
+                {
+                     ImagenPeliTexBox.Text=openFileDialog.FileName ;
+                }
+            }
         }
     }
 }
